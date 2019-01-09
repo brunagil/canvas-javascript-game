c43c585f6655c2e8902be34030ceaebd3ef34e71
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2019-01-09 20:36:19 -0200

    create hero

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const ENEMIES_STORE = [];
 const ENEMIES_SIZE = 50;
 const ENEMIES_COLORS = [
-  'red', 'blue', 'yellow', 'white', 'grey',
+  'red', 'blue', 'yellow', 'white',
   'green', 'purple', 'navy', 'silver', 'olive',
   'lime', 'fuchsia', 'teal', 'aqua', 'maroon'
 ];
+const HERO_SIZE = ENEMIES_SIZE;
+const HERO_COLOR = 'white';
 
 // Getting the DOM element.
 const canvas = document.getElementById('my-canvas');
@@ -37,6 +39,27 @@ class Enemy {
   }
 }
 
+// Our Hero class.
+class Hero {
+  constructor() {
+    this.x = 0;
+    this.y = canvas.height - HERO_SIZE;
+    this.width = HERO_SIZE;
+    this.height = HERO_SIZE;
+    this.color = HERO_COLOR;
+  }
+
+  draw() {
+    // Prevent our hero from going beyond the available area.
+    if (this.x < 0) this.x = 0;
+    if (this.x > canvas.width - HERO_SIZE) this.x = canvas.width - HERO_SIZE;
+
+    // Drawing the hero itself.
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, HERO_SIZE, HERO_SIZE);  
+  }
+}
+
 // This function just instantiate one enemy in a random x position and add it to the array of enemies.
 const createEnemy = () => {
   // Each 50 frames we create a new enemy.
@@ -54,6 +77,9 @@ const drawEnemies = () => {
   ENEMIES_STORE.forEach(enemy => enemy.draw());
 }
 
+// We just need one hero, so let's instantiate it.
+const ourHero = new Hero();
+
 // Canvas cleaner.
 const resetCanvas = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -65,6 +91,9 @@ const render = () => {
   // Incremeting frames for each loop.
   frames += 1;
 
+  // Drawing our hero.
+  ourHero.draw();
+
   // Intantiate one new enemy at a random x position and add it to the enemies array.
   createEnemy();
 
@@ -72,8 +101,103 @@ const render = () => {
   drawEnemies();
 }
 
+// Keyboard listener to check if the user press arrows keys.
+window.addEventListener('keydown', (e) => {
+  // Left arrow key.
+  if (e.keyCode === 37) {
+    if (ourHero.x <= 0) return;
+    ourHero.x -= HERO_SIZE;
+  }
+  // Right arrow key.
+  if (e.keyCode === 39) {
+    if (ourHero.x >= canvas.width - HERO_SIZE) return;
+    ourHero.x += HERO_SIZE;
+  }    
+});
+
 // Starting looper.
 looper = setInterval(render, 0);
+
+
+// Para criar a chuva de quadrados 
+// // Some app setup.
+// const ENEMIES_STORE = [];
+// const ENEMIES_SIZE = 50;
+// const ENEMIES_COLORS = [ // constante de cores que será sorteado
+//   'red', 'blue', 'yellow', 'white', 'grey',
+//   'green', 'purple', 'navy', 'silver', 'olive',
+//   'lime', 'fuchsia', 'teal', 'aqua', 'maroon'
+// ];
+
+// // Getting the DOM element.
+// const canvas = document.getElementById('my-canvas');
+
+// // Getting the 2d context.
+// const ctx = canvas.getContext('2d');
+
+// // Our looper control.
+// let looper;
+
+// // Animation frames counter.
+// let frames = 0;
+
+// // Our class that will generate enemies instances.
+// // Orientação a objeto 
+// class Enemy {
+//   constructor(x) {
+//     this.x = x;
+//     this.y = 0;
+//     this.width = ENEMIES_SIZE;
+//     this.height = ENEMIES_SIZE;
+//     // Getting a random color when the object is instantiated.
+//     this.color = ENEMIES_COLORS[Math.floor(Math.random() * ENEMIES_COLORS.length)];
+//   }
+
+//   draw() {
+//     this.y += 1;
+//     ctx.fillStyle = this.color;
+//     ctx.fillRect(this.x, this.y, ENEMIES_SIZE, ENEMIES_SIZE);  
+//   }
+// }
+
+// // This function just instantiate one enemy in a random x position and add it to the array of enemies.
+// // cria um novo inimigo a cada 50 quadros percorridos
+// const createEnemy = () => {
+//   // Each 50 frames we create a new enemy.
+//   if (frames % ENEMIES_SIZE === 0) {
+//     // Set enemy x coordinate from 0 to 450.
+//     const x = Math.floor(Math.random() * 10) * ENEMIES_SIZE;
+//     // Adding the enemy to the array of enemies.
+//     ENEMIES_STORE.push(new Enemy(x)); //armazena o inimigo que foi criado  //pesquisar estrutura de dados 
+//   }
+// }
+
+// // This functions performs a loop in the enemies array and draw each enemy.
+// const drawEnemies = () => {
+//   // Drawing all enemies.
+//   ENEMIES_STORE.forEach(enemy => enemy.draw());  //para cada inimigo, chama a função draw
+// }
+
+// // Canvas cleaner.
+// const resetCanvas = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+// // Each loop we call render function.
+// const render = () => {
+//   // We clean everything in the canvas.
+//   resetCanvas();
+
+//   // Incremeting frames for each loop.
+//   frames += 1;
+
+//   // Intantiate one new enemy at a random x position and add it to the enemies array.
+//   createEnemy();
+
+//   // Draw all enemies available in the enemies array.
+//   drawEnemies();
+// }
+
+// // Starting looper.
+// looper = setInterval(render, 0);
 
 
 // EXEMPLO DE ANIMAÇÃO EM QUADRADOS CINZA 
