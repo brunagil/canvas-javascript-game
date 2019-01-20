00ca8fe92628021ebd810de5837033f94ef90364
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2019-01-20 00:18:44 -0200

    functional game with comments

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// se y do enemy > 500 (sai do canvas), ele sai do array 
+// a cada quadrado que passou pelo canvas, conta 1 
+
+
 // Some app setup.
 const ENEMIES_STORE = [];
 const ENEMIES_SIZE = 50;
@@ -7,7 +11,12 @@ const ENEMIES_COLORS = [
   'lime', 'fuchsia', 'teal', 'aqua', 'maroon'
 ];
 const HERO_SIZE = ENEMIES_SIZE;
-const HERO_COLOR = 'white';
+const HERO_COLOR = 'grey';
+const GAME_OVER_FONT = '20px Verdana';
+const GAME_OVER_COLOR = 'white';
+const GAME_OVER_TEXT = 'GAME OVER';
+const GAME_OVER_X = 185;
+const GAME_OVER_Y = 220;
 
 // Getting the DOM element.
 const canvas = document.getElementById('my-canvas');
@@ -35,7 +44,7 @@ class Enemy {
   draw() {
     this.y += 1;
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, ENEMIES_SIZE, ENEMIES_SIZE);  
+    ctx.fillRect(this.x, this.y, ENEMIES_SIZE, ENEMIES_SIZE);  //dá pra usar um cara pra usar uma imagem
   }
 }
 
@@ -58,6 +67,11 @@ class Hero {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, HERO_SIZE, HERO_SIZE);  
   }
+
+  //******/Checks the hero position against enemies position.
+  checkCollision(enemy) {
+    return (this.x < enemy.x + enemy.width) && (this.x + this.width > enemy.x) && (this.y < enemy.y + enemy.height) && (this.y + this.height > enemy.y);
+  }  
 }
 
 // This function just instantiate one enemy in a random x position and add it to the array of enemies.
@@ -77,6 +91,15 @@ const drawEnemies = () => {
   ENEMIES_STORE.forEach(enemy => enemy.draw());
 }
 
+//********* */ Colission checker.
+const collisionChecker = () => {
+  ENEMIES_STORE.forEach(enemy => { //looping no array de inimigos criados
+    if (ourHero.checkCollision(enemy)) { //testa a posição dele com a posição do heroi
+      gameOver();
+    }
+  })
+}
+
 // We just need one hero, so let's instantiate it.
 const ourHero = new Hero();
 
@@ -91,14 +114,17 @@ const render = () => {
   // Incremeting frames for each loop.
   frames += 1;
 
-  // Drawing our hero.
+  // Drawing our hero in the current position.
   ourHero.draw();
 
-  // Intantiate one new enemy at a random x position and add it to the enemies array.
+  // Instantiate one new enemy at a random x position and add it to the enemies array.
   createEnemy();
 
   // Draw all enemies available in the enemies array.
   drawEnemies();
+
+  // Collision checker.
+  collisionChecker();
 }
 
 // Keyboard listener to check if the user press arrows keys.
@@ -115,8 +141,137 @@ window.addEventListener('keydown', (e) => {
   }    
 });
 
+// Stop the looper and print game over message.
+const gameOver = () => {
+  clearInterval(looper);
+  ctx.font = GAME_OVER_FONT;
+  ctx.fillStyle = GAME_OVER_COLOR;
+  ctx.fillText(GAME_OVER_TEXT, GAME_OVER_X, GAME_OVER_Y);
+}
+
 // Starting looper.
 looper = setInterval(render, 0);
+
+// CRIA O HEROI 
+// // Some app setup.
+// const ENEMIES_STORE = [];
+// const ENEMIES_SIZE = 50;
+// const ENEMIES_COLORS = [
+//   'red', 'blue', 'yellow', 'white',
+//   'green', 'purple', 'navy', 'silver', 'olive',
+//   'lime', 'fuchsia', 'teal', 'aqua', 'maroon'
+// ];
+// const HERO_SIZE = ENEMIES_SIZE;
+// const HERO_COLOR = 'white';
+
+// // Getting the DOM element.
+// const canvas = document.getElementById('my-canvas');
+
+// // Getting the 2d context.
+// const ctx = canvas.getContext('2d');
+
+// // Our looper control.
+// let looper;
+
+// // Animation frames counter.
+// let frames = 0;
+
+// // Our class that will generate enemies instances.
+// class Enemy {
+//   constructor(x) {
+//     this.x = x;
+//     this.y = 0;
+//     this.width = ENEMIES_SIZE;
+//     this.height = ENEMIES_SIZE;
+//     // Getting a random color when the object is instantiated.
+//     this.color = ENEMIES_COLORS[Math.floor(Math.random() * ENEMIES_COLORS.length)];
+//   }
+
+//   draw() {
+//     this.y += 1;
+//     ctx.fillStyle = this.color;
+//     ctx.fillRect(this.x, this.y, ENEMIES_SIZE, ENEMIES_SIZE);  
+//   }
+// }
+
+// // Our Hero class.
+// class Hero {
+//   constructor() {
+//     this.x = 0;
+//     this.y = canvas.height - HERO_SIZE;
+//     this.width = HERO_SIZE;
+//     this.height = HERO_SIZE;
+//     this.color = HERO_COLOR;
+//   }
+
+//   draw() {
+//     // Prevent our hero from going beyond the available area.
+//     if (this.x < 0) this.x = 0;
+//     if (this.x > canvas.width - HERO_SIZE) this.x = canvas.width - HERO_SIZE;
+
+//     // Drawing the hero itself.
+//     ctx.fillStyle = this.color;
+//     ctx.fillRect(this.x, this.y, HERO_SIZE, HERO_SIZE);  
+//   }
+// }
+
+// // This function just instantiate one enemy in a random x position and add it to the array of enemies.
+// const createEnemy = () => {
+//   // Each 50 frames we create a new enemy.
+//   if (frames % ENEMIES_SIZE === 0) {
+//     // Set enemy x coordinate from 0 to 450.
+//     const x = Math.floor(Math.random() * 10) * ENEMIES_SIZE;
+//     // Adding the enemy to the array of enemies.
+//     ENEMIES_STORE.push(new Enemy(x));
+//   }
+// }
+
+// // This functions performs a loop in the enemies array and draw each enemy.
+// const drawEnemies = () => {
+//   // Drawing all enemies.
+//   ENEMIES_STORE.forEach(enemy => enemy.draw());
+// }
+
+// // We just need one hero, so let's instantiate it.
+// const ourHero = new Hero();
+
+// // Canvas cleaner.
+// const resetCanvas = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+// // Each loop we call render function.
+// const render = () => {
+//   // We clean everything in the canvas.
+//   resetCanvas();
+
+//   // Incremeting frames for each loop.
+//   frames += 1;
+
+//   // Drawing our hero.
+//   ourHero.draw();
+
+//   // Intantiate one new enemy at a random x position and add it to the enemies array.
+//   createEnemy();
+
+//   // Draw all enemies available in the enemies array.
+//   drawEnemies();
+// }
+
+// // Keyboard listener to check if the user press arrows keys.
+// window.addEventListener('keydown', (e) => {
+//   // Left arrow key.
+//   if (e.keyCode === 37) {
+//     if (ourHero.x <= 0) return;
+//     ourHero.x -= HERO_SIZE;
+//   }
+//   // Right arrow key.
+//   if (e.keyCode === 39) {
+//     if (ourHero.x >= canvas.width - HERO_SIZE) return;
+//     ourHero.x += HERO_SIZE;
+//   }    
+// });
+
+// // Starting looper.
+// looper = setInterval(render, 0);
 
 
 // Para criar a chuva de quadrados 
